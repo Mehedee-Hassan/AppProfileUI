767dec62714b50322f9986919cd673bed4c3d196
Mehedee-Hassan <mehedee.hassan@outlook.com>
2017-12-18 17:47:25 +0600

    image added

---
+++ app/src/main/java/com/animation2/test/mhr/testanimation2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.animation2.test.mhr.testanimation2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
